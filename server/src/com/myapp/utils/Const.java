3fa4e7448e0e1a5d8af11216a8b007de2cbb6ad2
siqihuang <siqihuang2014@gmail.com>
2015-11-22 16:09:51 -0500

    movie object and movie homepage

--- server/src/com/myapp/utils/Const.java
+++ server/src/com/myapp/utils/Const.java
@@ -2,6 +2,6 @@ package com.myapp.utils;
 
 public class Const 
 {
-	
-
+	public static final int ORDERBY_RATING=0,ORDERBY_TIME=1,ORDERBY_VOTES=2;//orderby
+	public static final int ID_SEARCH=0,NAME_SEARCH=1;
 }
